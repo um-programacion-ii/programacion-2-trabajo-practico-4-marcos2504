1529bf32f9bea4466c0a3ac9953d35676d2c6691
marcos2504 <marcosibarra1234@gmail.com>
2025-05-13 23:41:04 -0300

    Crear los endpoints para la clase usuario

---
+++ Gestion_biblioteca/src/main/java/ar/edu/um/programacion2/Gestion_biblioteca/controller/UsuarioController.java
@@ -0,0 +1,45 @@
+package ar.edu.um.programacion2.Gestion_biblioteca.controller;
+
+
+import ar.edu.um.programacion2.Gestion_biblioteca.model.Usuario;
+import ar.edu.um.programacion2.Gestion_biblioteca.service.UsuarioService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @GetMapping
+    public List<Usuario> obtenerTodos() {
+        return usuarioService.obtenerTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Usuario obtenerPorId(@PathVariable Long id) {
+        return usuarioService.buscarPorId(id);
+    }
+
+    @PostMapping
+    public Usuario crear(@RequestBody Usuario usuario) {
+        return usuarioService.guardar(usuario);
+    }
+
+    @PutMapping("/{id}")
+    public Usuario actualizar(@PathVariable Long id, @RequestBody Usuario usuario) {
+        return usuarioService.actualizar(id, usuario);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable Long id) {
+        usuarioService.eliminar(id);
+    }
+}
+
